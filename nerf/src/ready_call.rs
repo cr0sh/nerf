194244a22e26f0f1d3e29e20557cabc40ae20367
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2022-12-09 23:40:58 +0900

    nerf: Add note about !Send-ness of ready_call

--- nerf/src/ready_call.rs
+++ nerf/src/ready_call.rs
@@ -3,6 +3,8 @@ use tower::{util::Oneshot, Service, ServiceExt};
 /// A trait providing [`ReadyCall::ready_call`] to [`Service`]s.
 pub trait ReadyCall<Request>: Service<Request> + Sized {
     /// Calls the service when the service is ready to process the request.
+    ///
+    /// Note: This may affect the calling future to be !Send because of a mutable borrowing
     fn ready_call(&mut self, req: Request) -> Oneshot<&mut Self, Request> {
         self.oneshot(req)
     }
