cc1a54d01f0096a2b66a6fc42f78b7d5b685b496
Nam Junghyun <13118114+cr0sh@users.noreply.github.com>
2022-12-31 13:02:21 +0900

    exchanges/common: Remove types.rs

--- nerf-exchanges/src/common/mod.rs
+++ nerf-exchanges/src/common/mod.rs
@@ -1,7 +1,5 @@
 //! Common types across various exchanges.
 
-mod types;
-
 use std::{convert::Infallible, fmt::Display, future::Future, pin::Pin, str::FromStr};
 
 use rust_decimal::Decimal;
@@ -11,7 +9,11 @@ use thiserror::Error;
 use nerf::{ClientService, ReadyCall};
 use tower::buffer::Buffer;
 
-pub use self::types::*;
+/// Conversion into common types.
+pub trait IntoCommon {
+    type Output;
+    fn into_common(self) -> Self::Output;
+}
 
 pub type Asset = String;
 
@@ -130,6 +132,74 @@ impl Display for MarketKind {
 pub enum Side {
     Buy,
     Sell,
+}
+
+#[derive(Clone, Debug)]
+pub struct Ticker {
+    bid_price: Decimal,
+    ask_price: Decimal,
+}
+
+impl Ticker {
+    pub fn new(bid_price: Decimal, ask_price: Decimal) -> Self {
+        Self {
+            bid_price,
+            ask_price,
+        }
+    }
+
+    pub fn bid_price(&self) -> Decimal {
+        self.bid_price
+    }
+
+    pub fn ask_price(&self) -> Decimal {
+        self.ask_price
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Orderbook {
+    bids: Vec<OrderbookItem>,
+    asks: Vec<OrderbookItem>,
+}
+
+impl Orderbook {
+    pub fn new(bids: Vec<OrderbookItem>, asks: Vec<OrderbookItem>) -> Self {
+        Self { bids, asks }
+    }
+
+    pub fn bids(&self) -> &[OrderbookItem] {
+        &self.bids
+    }
+    pub fn asks(&self) -> &[OrderbookItem] {
+        &self.asks
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct OrderbookItem {
+    pub price: Decimal,
+    pub quantity: Decimal,
+}
+
+impl OrderbookItem {
+    pub fn new(price: Decimal, quantity: Decimal) -> Self {
+        Self { price, quantity }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Trade {
+    pub price: Decimal,
+    pub quantity: Decimal,
+    pub taker_side: Side,
+    pub quantity_units: TradeQuantityUnits,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum TradeQuantityUnits {
+    Base,
+    Quote,
 }
 
 #[derive(Debug, PartialEq, Eq)]
